2f122d9b6e3b4ab5bf7a818d8c36736959b004fc
Juan Gomez <j.gomez@drexed.com>
2015-08-24 11:38:12 -0400

    fix model generator

--- lib/generators/rails/model_generator.rb
+++ lib/generators/rails/model_generator.rb
@@ -4,6 +4,12 @@ module ActiveRecord
   module Generators
     class ModelGenerator
 
+      def create_migration_file
+        return unless options[:migration] && options[:parent].nil?
+        attributes.each { |a| a.attr_options.delete(:index) if a.reference? && !a.has_index? } if options[:indexes] == false
+        migration_template "../../migration/templates/create_table_migration.rb", "db/migrate/create_#{table_name}.rb"
+      end
+
       def generate_locale_file
         invoke("locale:model", [name]) if ActiveGenerator.configuration.autoload_model_generator_locale
       end

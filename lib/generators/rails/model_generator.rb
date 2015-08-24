373e41518f402990dff8525fdcef1e7831f12886
Juan Gomez <j.gomez@drexed.com>
2015-08-24 11:41:24 -0400

    fix model generator

--- lib/generators/rails/model_generator.rb
+++ lib/generators/rails/model_generator.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     class ModelGenerator
 
       def create_migration_file
-        return unless options[:migration] && options[:parent].nil?
+        # return unless options[:migration] && options[:parent].nil?
         attributes.each { |a| a.attr_options.delete(:index) if a.reference? && !a.has_index? } if options[:indexes] == false
         migration_template "../../migration/templates/create_table_migration.rb", "db/migrate/create_#{table_name}.rb"
       end

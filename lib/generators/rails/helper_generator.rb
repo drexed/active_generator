f640cde340b2ab9642b6ab879d46ed2f7cca24c2
Juan Gomez <j.gomez@drexed.com>
2015-08-26 20:54:26 -0400

    test

--- lib/generators/rails/helper_generator.rb
+++ lib/generators/rails/helper_generator.rb
@@ -8,6 +8,10 @@ module Rails
         invoke("locale:helper", [name]) if ActiveGenerator.configuration.autoload_helper_generator_locale
       end
 
+      def generate_test_file
+        invoke("rspec:helper", [name])
+      end
+
     end
   end
 end

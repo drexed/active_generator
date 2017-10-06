4d4b1a77a00b35e53b64a5e91140a5f7415b01fd
Juan Gomez <j.gomez@drexed.com>
2017-10-06 15:26:15 -0400

    clean up

--- spec/lib/generators/environment/environment_generator_spec.rb
+++ spec/lib/generators/environment/environment_generator_spec.rb
@@ -10,24 +10,31 @@ describe EnvironmentGenerator, type: :generator do
     run_generator(%w(staging production))
   end
 
-  it 'to be true' do
-    sample_path = 'spec/lib/generators/tmp/config/environments/sample.rb'
+  let(:sample_path) { 'spec/lib/generators/tmp/config/environments/sample.rb' }
+  let(:staging_path) { 'spec/lib/generators/tmp/config/environments/staging.rb' }
 
-    expect_file = File.read('spec/support/generators/environments/sample.rb')
-    sample_file = File.read(sample_path)
+  describe '#generator' do
+    it 'to be true when sample file exists' do
+      expect(File.exist?(sample_path)).to eq(true)
+    end
 
-    expect(File.exist?(sample_path)).to eq(true)
-    expect(sample_file).to eq(expect_file)
-  end
+    it 'to be the same as the expected file' do
+      expect_file = File.read('spec/support/generators/environments/sample.rb')
+      sample_file = File.read(sample_path)
 
-  it 'to be true' do
-    sample_path = 'spec/lib/generators/tmp/config/environments/staging.rb'
+      expect(sample_file).to eq(expect_file)
+    end
 
-    expect_file = File.read('spec/support/generators/environments/staging.rb')
-    sample_file = File.read(sample_path)
+    it 'to be true when staging file exists' do
+      expect(File.exist?(staging_path)).to eq(true)
+    end
 
-    expect(File.exist?(sample_path)).to eq(true)
-    expect(sample_file).to eq(expect_file)
+    it 'to be the same as the expected file' do
+      expect_file = File.read('spec/support/generators/environments/staging.rb')
+      sample_file = File.read(staging_path)
+
+      expect(sample_file).to eq(expect_file)
+    end
   end
 
 end
--- spec/lib/generators/view/view_generator_spec.rb
+++ spec/lib/generators/view/view_generator_spec.rb
@@ -10,24 +10,26 @@ describe ViewGenerator, type: :generator do
     run_generator(%w(v1/social/twitter index))
   end
 
-  it 'to be true' do
-    locale_path = 'spec/lib/generators/tmp/config/locales/en/views/sample.yml'
-    sample_path = 'spec/lib/generators/tmp/app/views/sample'
+  describe '#generator' do
+    it 'to be true' do
+      locale_path = 'spec/lib/generators/tmp/config/locales/en/views/sample.yml'
+      sample_path = 'spec/lib/generators/tmp/app/views/sample'
 
-    expect(File.exist?(locale_path)).to eq(true)
-    expect(File.directory?(sample_path)).to eq(true)
-  end
+      expect(File.exist?(locale_path)).to eq(true)
+      expect(File.directory?(sample_path)).to eq(true)
+    end
 
-  it 'to be true' do
-    locale_path = 'spec/lib/generators/tmp/config/locales/en/views/v1/social/twitter.yml'
-    sample_path = 'spec/lib/generators/tmp/app/views/v1/social/twitter/index.html.erb'
+    it 'to be true' do
+      locale_path = 'spec/lib/generators/tmp/config/locales/en/views/v1/social/twitter.yml'
+      sample_path = 'spec/lib/generators/tmp/app/views/v1/social/twitter/index.html.erb'
 
-    expect_file = File.read('spec/support/generators/views/index.html.erb')
-    sample_file = File.read(sample_path)
+      expect_file = File.read('spec/support/generators/views/index.html.erb')
+      sample_file = File.read(sample_path)
 
-    expect(File.exist?(locale_path)).to eq(true)
-    expect(File.exist?(sample_path)).to eq(true)
-    expect(sample_file).to eq(expect_file)
+      expect(File.exist?(locale_path)).to eq(true)
+      expect(File.exist?(sample_path)).to eq(true)
+      expect(sample_file).to eq(expect_file)
+    end
   end
 
 end
